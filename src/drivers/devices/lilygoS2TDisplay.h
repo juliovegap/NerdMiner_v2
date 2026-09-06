5c3f192f699a0578770794b68b83d25f3e3034fe
juliovegap <4875691+juliovegap@users.noreply.github.com>
2026-09-06 23:22:23 +0200

    Rename header guards for lilygoTDisplayS2T8

--- src/drivers/devices/lilygoS2TDisplay.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LILYGO_S2_T_DISPLAY_H
-#define _LILYGO_S2_T_DISPLAY_H
-
-#define PIN_BUTTON_1 0
-#define PIN_BUTTON_2 35
-
-//#define ST7789_DRIVER
-
-#endif
---
+++ src/drivers/devices/lilygoTDisplayS2T8.h
@@ -0,0 +1,8 @@
+#ifndef _LILYGO_T_DISPLAY_S2_T8_H
+#define _LILYGO_T_DISPLAY_S2_T8_H
+
+#define PIN_BUTTON_1 0
+
+//#define ST7789_DRIVER
+
+#endif
